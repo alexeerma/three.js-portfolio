c296b6ec425569d946aaecd1b998f0abe66f1339
alexeerma <aleksander.eerma@hotmail.com>
2023-01-28 23:07:49 +0200

    Some text tweaks

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,31 +6,23 @@ const Hero = () => (
     
   <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
     <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
-       <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2">
-          <img src={discount} alt="discount" className="w-[32px] h-[32px]" />
-          <p className={`${styles.paragraph} ml-2`}>
-           <span className="text-white">20%</span> Discount for {""}
-           <span className="text-white">1 Month</span> Account
-          </p>
-       </div>
+
 
        <div className="flex flex-row justify-between items-center w-full">
-          <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100.8px] leading-[75px]">
-            The Next <br className="sm:block hidden" />{" "}
-            <span className="text-gradient">Generation</span>{" "}
+          <h1 className="flex-1 font-poppins font-semibold tracking-tight ss:text-[72px] text-[52px] text-white ss:leading-[100.8px] leading-[35px]">
+            Aleksander <br className="sm:block hidden" />{" "}
+            <span className="text-gradient tracking-widest">Eerma</span>{" "}
           </h1>
           <div className="ss:flex hidden md:mr-4 mr-0">
             <GetStarted />
           </div>
         </div>
 
-        <h1 className="font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100.8px] leading-[75px] w-full">
-          Payment Method.
+        <h1 className="font-poppins font-medium tracking-tight ss:text-[50px] text-[52px] text-white ss:leading-[100.8px] leading-[75px] w-full">
+          Athlete / Freelance Developer
         </h1>
-        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
-          Our team of experts uses a methodology to identify the credit cards
-          most likely to fit your needs. We examine annual percentage rates,
-          annual fees.
+        <p className={`${styles.paragraph} text-[15px] max-w-[470px] mt-25`}>
+          Hi! I am Alex and I like to code different projects. See what I focus on mainly from <span className='font-bold'>Tech</span>
         </p>
       </div>
 
--- src/style.js
+++ src/style.js
@@ -1,7 +1,7 @@
 const styles = {
     boxWidth: "xl:max-w-[1280px] w-full",
   
-    heading2: "font-poppins font-semibold xs:text-[40px] text-[32px] text-white xs:leading-[76.8px] leading-[66.8px] w-full",
+    heading2: "font-poppins font-semibold xs:text-[40px] text-[32px] text-white xs:leading-[76.8px] leading-[66.8px] w-2/3",
     paragraph: "font-poppins font-normal text-dimWhite text-[18px] leading-[30.8px]",
   
     flexCenter: "flex justify-center items-center",
