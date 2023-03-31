1982002f1684989cdb7cc2eb40db4aebc02a6810
alexeerma <aleksander.eerma@hotmail.com>
2023-03-31 18:31:55 +0300

    Trying to add extra new gltf models

--- src/components/canvas/Galazy.jsx
+++ src/components/canvas/Galazy.jsx
@@ -4,41 +4,44 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
 import CanvasLoader from "../Loader";
 
-const Galazy = () => {
-    const galazy = useGLTF("./galazy/scene.gltf");
+export function Model(props) {
+  const group = useRef();
+  const { nodes, materials, animations } = useGLTF("/galazy.gltf");
+  const { actions } = useAnimations(animations, group);
+  return (
+    <group ref={group} {...props} dispose={null}>
+      <group name="Sketchfab_Scene">
+        <group name="Sketchfab_model" rotation={[-Math.PI / 2, 0, 0]}>
+          <group name="root">
+            <group name="GLTF_SceneRootNode" rotation={[Math.PI / 2, 0, 0]}>
+              <group name="Sphere001_0" scale={1.13}>
+                <mesh
+                  name="Object_4"
+                  castShadow
+                  receiveShadow
+                  geometry={nodes.Object_4.geometry}
+                  material={materials.material}
+                />
+              </group>
+              <group name="Armature_5">
+                <group name="GLTF_created_0">
+                  <primitive object={nodes.GLTF_created_0_rootJoint} />
+                  <group name="Sphere_4" />
+                  <skinnedMesh
+                    name="Object_9"
+                    geometry={nodes.Object_9.geometry}
+                    material={materials.base}
+                    skeleton={nodes.Object_9.skeleton}
+                  />
+                </group>
+              </group>
+            </group>
+          </group>
+        </group>
+      </group>
+    </group>
+  );
+}
 
-return (
-        <primitive object={galazy.scene} scale={2.5} position-y={0} rotation-y={0} />
-      );
-    };
-
-const GalazyCanvas = () => {
-    return (
-      <Canvas
-        shadows
-        frameloop='demand'
-        dpr={[1, 2]}
-        gl={{ preserveDrawingBuffer: true }}
-        camera={{
-          fov: 45,
-          near: 0.1,
-          far: 200,
-          position: [-4, 3, 6],
-        }}
-      >
-        <Suspense fallback={<CanvasLoader />}>
-          <OrbitControls
-            autoRotate
-            enableZoom={false}
-            maxPolarAngle={Math.PI / 2}
-            minPolarAngle={Math.PI / 2}
-          />
-          <Galazy />
+useGLTF.preload("/galazy.glb");
   
-          <Preload all />
-        </Suspense>
-      </Canvas>
-    );
-  };
-  
-  export default GalazyCanvas;
