fefa9a2be30af3f372decf922ae430f3fcc60e99
alexeerma <aleksander.eerma@hotmail.com>
2023-05-02 12:45:00 +0300

    Minor styling changes

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -71,14 +71,14 @@ const Works = () => {
   return (
     <>
       <motion.div variants={textVariant()}>
-        <p className={`${styles.sectionSubText} `}>My work</p>
-        <h2 className={`${styles.sectionHeadText}`}>Projects.</h2>
+        <p className={`${styles.sectionSubText} text-right`}>My work</p>
+        <h2 className={`${styles.sectionHeadText} text-right`}>Projects.</h2>
       </motion.div>
 
-      <div className='w-full flex'>
+      <div className='w-full flex justify-end'>
         <motion.p
           variants={fadeIn("", "", 0.1, 1)}
-          className='mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]'
+          className='mt-3 text-secondary text-[17px] max-w-3xl leading-[30px] text-right right-0'
         >
           Following projects showcases my skills and experience through
           real-world examples of my work. Each project is briefly described with
